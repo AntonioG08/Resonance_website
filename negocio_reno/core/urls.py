86264b0d9b0cd0748608a7f41d6a58efb9c51a18
T0ny6969 <togoruiz08@hotmail.com>
2024-11-30 23:18:02 -0600

    Webpage working V1.0

---
+++ negocio_reno/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from core import views
+
+
+app_name = 'core'
+urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
+    path('about-us/', views.Us.as_view(), name='us'),
+    path('gallery/', views.Gallery.as_view(), name='gallery'),
+]
---
+++ negocio_reno/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+
+#Class used to render the 'home' template which is the main page
+class Home(View):
+    def get(self, request):
+        return render(request, 'core/home.html')
+
+
+#Class used to render the 'Us' template which contains information about us
+class Us(View):
+    def get(self, request):
+        return render(request, 'core/us.html')
+    
+
+#Class used to render the 'Us' template which contains information about us
+class Gallery(View):
+    def get(self, request):
+        return render(request, 'core/gallery.html')
