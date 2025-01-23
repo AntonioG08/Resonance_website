807120bd4f0492d75ce4e5bbbc85d3692ce275d4
T0ny6969 <togoruiz08@hotmail.com>
2025-01-22 22:56:24 -0600

    V2.1 - DB mainly ready, able to create services from admin panel

--- negocio_reno/negocio_reno/urls.py
+++ negocio_reno/negocio_reno/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')), #Used to import all the URLs paths of 'Blog'
     path('services/', include('services.urls')) #Used to import all the URLs paths of 'Services'
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                          document_root=settings.MEDIA_ROOT)
--- negocio_reno/services/views.py
+++ negocio_reno/services/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, ListView
+from services import models #Usado para importar todos los modelos de la app 'Services' 
 
 
 #Clase utilizada para cargar el template 'gallery.html' donde se muestran los servicios del negocio
-class Gallery(View):
-    def get(self, request):
-        return render(request, 'services/gallery.html')
+class Gallery(ListView):
+    template_name = 'services/gallery.html'
+    model = models.Services
+    context_object_name = 'servicios'
+    
